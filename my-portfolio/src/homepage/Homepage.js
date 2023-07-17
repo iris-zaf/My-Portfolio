305f5aaac9e71226721834b147ee82ee07f15c9d
Iris <iriskalogirou1@gmail.com>
2023-07-17 22:48:51 +0300

    added chit-chat app

--- my-portfolio/src/homepage/Homepage.js
+++ my-portfolio/src/homepage/Homepage.js
@@ -38,7 +38,11 @@ function Home() {
       </motion.div>
       <ImageTwo />
       {/* <ImageThree /> */}
-      <MDBRow className="row-cols-1 row-cols-md-3 m-5" id="content">
+      <MDBRow
+        className="row-cols-1 row-cols-md-4
+       m-5"
+        id="content"
+      >
         {" "}
         <MDBCol className="smallCardContainer">
           {" "}
@@ -122,6 +126,43 @@ function Home() {
                 </large>
                 <large className="text-muted imageContainer">
                   <img src={mongodb} alt="mongodb" style={{ width: "25px" }} />
+                </large>
+              </MDBCardFooter>
+            </MDBCard>
+          </motion.div>
+        </MDBCol>
+        <MDBCol className="smallCardContainer">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 2 }}
+          >
+            <MDBCard className="h-100 smallCard">
+              <MDBCardBody>
+                <MDBCardTitle>
+                  {" "}
+                  <a
+                    href="https://chit-chat-iris.netlify.app"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Chit-Chat(In Production)
+                  </a>
+                </MDBCardTitle>
+                <MDBCardText>
+                  Chit-Chat helps users interact via text messages, videos and
+                  many more. Each user has their own profile with the saved
+                  chats and the ability to create new ones.
+                </MDBCardText>
+              </MDBCardBody>
+              <MDBCardFooter>
+                <small className="text-muted">Technologies used:</small>
+                <br />
+                <large className="text-muted ">
+                  <i class="fa-brands fa-react fa-xl p-2"></i>
+                </large>
+                <large className="text-muted ">
+                  <i class="fa-brands fa-node fa-xl p-2"></i>
                 </large>
               </MDBCardFooter>
             </MDBCard>
